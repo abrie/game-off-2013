a66f14be6c8a223ff7a8a53a4fd2e3ef77add1f2
abrie <abrie@errante.local>
2013-11-01 16:10:29 +0000

    2 puzzle objects, side by side.

--- main.js
+++ main.js
@@ -3,9 +3,13 @@
 require(['puzzle', 'scene', 'player'],function( puzzle, scene, player ) {
     // puzzleObject exposes an interface of: .model and .pickables
     var puzzleObject = new puzzle.PuzzleModel();
+    puzzleObject.model.position.x = 100;
+    var puzzleObject2 = new puzzle.PuzzleModel();
+    puzzleObject2.model.position.x = -100;
 
     // scene.add interprets the interface of .model and .pickables
     scene.add( puzzleObject );
+    scene.add( puzzleObject2 );
 
     // TODO: the player object should also exposes a pickables[] for consistency
     var player = new player.Player();
