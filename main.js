1a3825070e9eae7341353ae12892e92184af42df
abrie <abrie@errante.local>
2013-11-01 13:54:19 +0000

    refactor: move player model generation to module

--- main.js
+++ main.js
@@ -1,32 +1,14 @@
 "use strict";
 
-require(['puzzle', 'scene', 'colors'],function( puzzle, scene, colors ) {
-
+require(['puzzle', 'scene', 'player'],function( puzzle, scene, player ) {
     // puzzleObject exposes an interface of: .model and .pickables
     var puzzleObject = new puzzle.PuzzleModel();
 
     // scene.add interprets the interface of .model and .pickables
     scene.add( puzzleObject );
 
-    function Player() {
-        var geometry = new THREE.CubeGeometry(
-            20,20,20
-        );
-
-        var material = new THREE.MeshPhongMaterial({
-            color: colors.palette[0],
-        });
-
-        var mesh = new THREE.Mesh( geometry, material );
-        var object = new THREE.Object3D();
-        object.add(mesh);
-        
-        return {
-            model:object,
-        }
-    }
-
-    var player = new Player();
+    // TODO: the player object should also exposes a pickables[] for consistency
+    var player = new player.Player();
     puzzleObject.addPlayer(player);
     puzzleObject.doAction(4);
 });
