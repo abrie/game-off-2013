e30cdfd84ae7cf3272873dbd23fd92be12a7ef47
abrie <abrie@errante.local>
2013-11-01 08:00:03 +0000

    refactor: use a constructor for PuzzlePieceModel

--- main.js
+++ main.js
@@ -11,45 +11,46 @@ require(['puzzle','three.min'],function(puzzle) {
         return puzzleSize/puzzleDim * (2*v - puzzleDim + 1) / 2;
     }
 
-    function PuzzlePiece(index) {
-        var model = createPuzzlePieceModel(index);
-        setIndex(index);
-
-        function setIndex(index) {
-            model.position.x = puzzleCoordinate( index % puzzleDim ); 
-            model.position.y = puzzleCoordinate( Math.floor( index / puzzleDim ) )
-            model.position.z = 0;
-        }
-        return {
-            setIndex:setIndex,
-            model:model
-        }
-    }
-
-    for( var i = 0; i < puzzleDim*puzzleDim; i++ ) {
-        if( i === Math.floor( puzzleDim*puzzleDim/2 ) ) {
-            puzzlePieces.push( false );
-        }
-        else {
-            puzzlePieces.push( new PuzzlePiece(i) );
-        }
-    }
-
-    var puzzleObject = new puzzle.Puzzle( puzzlePieces );
-    puzzleObject.onIndiciesSwapped( function(i,j) {
-        if( puzzlePieces[i] )
-            puzzlePieces[i].setIndex(i);
-        if( puzzlePieces[j] )
-            puzzlePieces[j].setIndex(j);
-    });
-
-    function createPuzzlePieceModel( index ) {
+    function PuzzlePieceModel( index ) {
         var geometry = new THREE.CubeGeometry( puzzleSize/puzzleDim-1, puzzleSize/puzzleDim-1, puzzleSize/puzzleDim/2 );
         var material = new THREE.MeshPhongMaterial( { color: colorPalette[index % colorPalette.length]} );
         var mesh = new THREE.Mesh( geometry, material );
 
         return mesh;
     }
+
+    function PuzzlePiece( index ) {
+        var model = new PuzzlePieceModel( index );
+        setIndex( index );
+
+        function setIndex(index) {
+            model.position.x = puzzleCoordinate( index % puzzleDim ); 
+            model.position.y = puzzleCoordinate( Math.floor( index / puzzleDim ) )
+            model.position.z = 0;
+        }
+
+        return {
+            setIndex:setIndex,
+            model:model
+        }
+    }
+
+    for( var index = 0; index < puzzleDim*puzzleDim; index++ ) {
+        if( index === Math.floor( puzzleDim*puzzleDim/2 ) ) {
+            puzzlePieces.push( false );
+        }
+        else {
+            puzzlePieces.push( new PuzzlePiece( index ) );
+        }
+    }
+
+    var puzzleObject = new puzzle.Puzzle( puzzlePieces );
+    puzzleObject.onIndiciesSwapped( function( i, j ) {
+        if( puzzlePieces[i] )
+            puzzlePieces[i].setIndex(i);
+        if( puzzlePieces[j] )
+            puzzlePieces[j].setIndex(j);
+    });
 
     function init() {
         camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
