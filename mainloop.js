f07ce38af275d1ea40b678451b88d775af9715c8
abrie <abrie@errante.local>
2013-11-05 17:03:23 +0000

    incremental commit: building lookAt functions for straw

--- mainloop.js
+++ mainloop.js
@@ -22,37 +22,47 @@ define(['assets', 'scene', 'sceneNoAR', 'puzzle', 'tween.min', 'three.min'], fun
     }
 
     function createThing( ) {
-        var geometry = new THREE.CylinderGeometry( 3.0, 3.0, 200 );
+        var geometry = new THREE.CylinderGeometry( 30.0, 30.0, 100 );
 
         var quaternion = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3(1,0,0), -Math.PI/2 );
 
         var tMatrix = new THREE.Matrix4();
-        tMatrix.makeTranslation( 0, 100, 0);
+        tMatrix.makeTranslation( 0, -200, 0);
         geometry.applyMatrix( tMatrix );
 
         var rMatrix = new THREE.Matrix4();
         rMatrix.makeRotationFromQuaternion( quaternion );
         geometry.applyMatrix( rMatrix );
 
-        var material = new THREE.MeshPhongMaterial({
+        var material = new THREE.MeshNormalMaterial({
             color: 0xFFFFFF,
        });
 
         var mesh = new THREE.Mesh( geometry, material );
         mesh.matrixAutoUpdate = false;
 
+        function r(a) { return Math.random()*a-a/2; }
+
         mesh.transform = function(m) {
             mesh.matrix.fromArray(m);
+            var target = new THREE.Vector3();
+            target.getPositionFromMatrix( mesh.matrix );
+            target.x += r(20);
+            target.y += r(20);
+            target.z += r(20);
+            var eye = new THREE.Vector3(0,0,0);
+            mesh.matrix.lookAt( eye, target, mesh.up );
             mesh.matrixWorldNeedsUpdate = true;
         };
 
+        console.log( mesh.matrix );
         return mesh;
     }
 
     function addThing( pz ) {
 
         function addInternal() {
-            var position = new THREE.Vector3( 0, 0, 40);
+            var position = new THREE.Vector3( 0, 0, -40);
             var object = createThing();
             object.position = position;
             //object.lookAt( sceneObject.getCameraPosition() );
