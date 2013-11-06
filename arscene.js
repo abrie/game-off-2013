9a33b2ddf6682838557907dc585ab61bb333431d
abrie <abrie@errante.local>
2013-11-06 07:27:22 +0000

    specify target element in constructor

--- arscene.js
+++ arscene.js
@@ -1,26 +1,29 @@
 "use strict";
 
 define(['picker','canvas','ardetector','arview','pitobject'], function(picker,canvas,ardetector,arview,pitobject) {
-    function Scene( video ) {
+    function Scene( element, video ) {
         var detectorCanvas = canvas.create( video.getDimensions() );
-
         var detector = ardetector.create( detectorCanvas );
         var view = arview.create( video.getDimensions(), detectorCanvas );
         view.setCameraMatrix( detector.getCameraMatrix( 5, 10000 ) );
-        document.body.appendChild( view.glCanvas );
+        element.appendChild( view.glCanvas );
 
         var objectPicker = new picker.Picker( view.getCamera(), view.glCanvas );
 
         // Create marker objects associated with the desired marker ID.
         var markerObjects = {
-            32: [new pitobject.PitObject({color:0x000000})], 
-            4: [new pitobject.PitObject({color:0x000000})],
+            32: [ new pitobject.PitObject({color:0x000000}) ], 
+            4: [ new pitobject.PitObject({color:0x000000}) ],
         };
 
         function update() {
             video.seek(1);
             detectorCanvas.update( video );
-            detector.detect( onMarkerCreated, onMarkerUpdated, onMarkerDestroyed );
+            detector.detect( 
+                onMarkerCreated, 
+                onMarkerUpdated, 
+                onMarkerDestroyed 
+            );
             view.update();
             view.render();
         }
