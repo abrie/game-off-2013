bcc0dfbd135135b8d318ffd1e72352fe75c1a6d0
abrie <abrie@errante.local>
2013-11-06 06:23:57 +0000

    refactor: make strawman module

--- mainloop.js
+++ mainloop.js
@@ -1,6 +1,6 @@
 "use strict";
 
-define(['assets', 'scene', 'sceneNoAR', 'puzzle', 'tween.min', 'three.min'], function( assets, scene, sceneNoAR, puzzle ){
+define(['assets', 'scene', 'puzzle', 'strawman', 'tween.min', 'three.min'], function( assets, scene, puzzle, strawman ){
 
     var sceneObject;
 
@@ -8,11 +8,6 @@ define(['assets', 'scene', 'sceneNoAR', 'puzzle', 'tween.min', 'three.min'], fun
         requestAnimationFrame( animate );
         TWEEN.update();
         sceneObject.update();
-        step+=dir; 
-        if( step > 100  || step < -100) {
-            dir*=-1;
-        }
-
     }
 
     var holds = [];
@@ -26,81 +21,16 @@ define(['assets', 'scene', 'sceneNoAR', 'puzzle', 'tween.min', 'three.min'], fun
         requestAnimationFrame( animate );
     }
 
-    var step = 100.0;
-    var dir = -1;
-    function createThing( ) {
-        var geometry = new THREE.CylinderGeometry( 5.0, 30.0, 100*6, 100 );
+    function addThing( pz ) {
+        var model = new strawman.Straw(); 
 
-        var quaternion = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3(1,0,0), -Math.PI/2 );
-
-        var tMatrix = new THREE.Matrix4();
-        tMatrix.makeTranslation( 0, -50*6, 0);
-        geometry.applyMatrix( tMatrix );
-
-        var rMatrix = new THREE.Matrix4();
-        rMatrix.makeRotationFromQuaternion( quaternion );
-        geometry.applyMatrix( rMatrix );
-
-        var material = new THREE.MeshNormalMaterial({
-            color: 0xFFFFFF,
-            side: THREE.DoubleSide,
-       });
-
-        var mesh = new THREE.Mesh( geometry, material );
-        mesh.matrixAutoUpdate = false;
-
-        function r(a) { return Math.random()*a-a/2; }
-
-        mesh.transform = function(m) {
-            mesh.matrix.fromArray(m);
-            var target = new THREE.Vector3();
-            target.getPositionFromMatrix( mesh.matrix );
-            var eye = new THREE.Vector3(0,0,0);
-            eye.x -= step;
-            var up = mesh.up.clone();
-            mesh.matrix.lookAt( eye, target, up );
-            mesh.matrixWorldNeedsUpdate = true;
+        var straw = {
+            model: model,
+            transform: model.transform,
+            pickables: [],
         };
 
-        return mesh;
-    }
-
-    function addThing( pz ) {
-
-        function addInternal() {
-            var position = new THREE.Vector3( 0, 0, -40);
-            var object = createThing();
-            object.position = position;
-            //object.lookAt( sceneObject.getCameraPosition() );
-
-            var thing = {
-                model: object,
-                transform: object.transform,
-                pickables: [],
-            };
-
-            //pz.model.add( thing.model );
-            sceneObject.add( pz.AR_ID, thing );
-        }
-
-        function addExternal() {
-            sceneObject.updateMatrixWorld();
-            var position = pz.getHolePosition();
-            var object = createThing();
-            object.position.x = position.x;
-            object.position.y = position.y;
-            object.position.z = position.z;
-            //object.lookAt( sceneObject.getCameraPosition() );
-
-            var thing = {
-                model: object,
-                pickables: [],
-            };
-
-            sceneObject.add( -1, thing );
-        }
-
-        addInternal();
+        sceneObject.add( pz.AR_ID, straw );
     }
 
     GLOBAL.add = function() {
---
+++ strawman.js
@@ -0,0 +1,42 @@
+"use strict";
+define([], function() {
+    function Straw() {
+        var geometry = new THREE.CylinderGeometry( 5.0, 30.0, 100*2, 100 );
+
+        var quaternion = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3(1,0,0), -Math.PI/2 );
+
+        var tMatrix = new THREE.Matrix4();
+        tMatrix.makeTranslation( 0, -50*2, 0);
+        geometry.applyMatrix( tMatrix );
+
+        var rMatrix = new THREE.Matrix4();
+        rMatrix.makeRotationFromQuaternion( quaternion );
+        geometry.applyMatrix( rMatrix );
+
+        var material = new THREE.MeshNormalMaterial({
+            color: 0xFFFFFF,
+            side: THREE.DoubleSide,
+       });
+
+        var mesh = new THREE.Mesh( geometry, material );
+        mesh.matrixAutoUpdate = false;
+
+        function r(a) { return Math.random()*a-a/2; }
+
+        mesh.transform = function(m) {
+            mesh.matrix.fromArray(m);
+            var target = new THREE.Vector3();
+            target.getPositionFromMatrix( mesh.matrix );
+            var eye = new THREE.Vector3(0,0,0);
+            var up = mesh.up.clone();
+            mesh.matrix.lookAt( eye, target, up );
+            mesh.matrixWorldNeedsUpdate = true;
+        };
+
+        return mesh;
+    }
+
+    return {
+        Straw:Straw,
+    }
+});
