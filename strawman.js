4a86d7b4beebbe6a71d19444fae3f55dfdcc24ff
abrie <abrie@errante.local>
2013-11-07 14:25:36 +0000

    incremental

--- strawman.js
+++ strawman.js
@@ -3,18 +3,19 @@ define([], function() {
     function Straw() {
 
         var points = new THREE.SplineCurve3([
-          new THREE.Vector3(0, 0, 0),
+          new THREE.Vector3(0, 120*2,40),
+          new THREE.Vector3(0, 120*2-10, 10),
           new THREE.Vector3(0, 100*2,0),
         ]);
 
         // points, ?, radius, facets, ? ?
-        var geometry = new THREE.TubeGeometry(points, 6,12,16, false, false);
+        var geometry = new THREE.TubeGeometry(points, 12,6,16, false, false);
 
         var quaternion = new THREE.Quaternion()
             .setFromAxisAngle( new THREE.Vector3(1,0,0), -Math.PI/2 );
 
         var tMatrix = new THREE.Matrix4();
-        tMatrix.makeTranslation( 0, -100*2, 0);
+        tMatrix.makeTranslation( 10, -215, -20);
         geometry.applyMatrix( tMatrix );
 
         var rMatrix = new THREE.Matrix4();
