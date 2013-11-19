4c83041eb76afba96c7565f515debe5becccd1ad
abrie <abrie@errante.local>
2013-11-19 17:49:44 +0000

    allow filters and scenes to be navigated with keys

---
+++ keys.js
@@ -0,0 +1,53 @@
+"use strict";
+define([], function() {
+    document.addEventListener( 'keydown', onDocumentKeyDown, false );
+    document.addEventListener( 'keyup', onDocumentKeyUp, false );
+
+    var states = {};
+    var actions = {
+        UP: [87, 38],
+        LEFT: [65, 37],
+        DOWN: [83, 40],
+        RIGHT: [68, 39]
+    };
+
+    function onDocumentKeyDown( event ) {
+        var keyCode = event.keyCode;
+        if( !states[keyCode] ) {
+            states[keyCode] = true;
+            onPress(keyCode);
+        }
+    }
+
+    function onDocumentKeyUp( event ) {
+        var keyCode = event.keyCode;
+        if( states[keyCode] ) {
+            states[keyCode] = false;
+            onRelease(keyCode);
+        }
+    }
+
+    function onPress(keyCode) {
+        function isKeycode(k) { return k === keyCode; }
+        for( var dir in actions ) {
+            if( actions[dir].some( isKeycode ) ) {
+                result.fireEvent(dir, true);
+            }
+        }
+    }
+
+    function onRelease(keyCode) {
+        function isKeycode(k) { return k === keyCode; }
+        for( var dir in actions ) {
+            if( actions[dir].some( isKeycode ) ) {
+                result.fireEvent(dir, false);
+            }
+        }
+    }
+
+    var result = {
+        fireEvent:undefined
+    };
+
+    return result;
+});
--- ui.js
+++ ui.js
@@ -1,30 +1,51 @@
 "use strict";
-define([], function() {
-    function makeButton(name) {
+define(['keys'], function( keys ) {
+    function makeButton( name ) {
         var element = document.createElement("button");
         element.innerHTML = name; 
         document.body.appendChild( element );
         return element;
     }
+
     var filterPreviousButton = makeButton("previous filter");
     var filterNextButton = makeButton("next filter");
     var scenePreviousButton = makeButton("previous scene");
     var sceneNextButton = makeButton("next scene");
 
+    var keyEventListeners = {};
+    function addKeyEventListener(event, callback) {
+        if( !keyEventListeners[event] ) {
+            keyEventListeners[event] = [];
+        }
+        keyEventListeners[event].push(callback);
+    }
+
+    keys.fireEvent = function(event, isDown) {
+        if( isDown && keyEventListeners[event] ) {
+            keyEventListeners[event].forEach( function(callback) {
+                callback();
+            });
+        }
+    };
+
     function addFilterNextListener( callback ) {
         filterNextButton.addEventListener( "click", callback );
+        addKeyEventListener("UP", callback);
     }
 
     function addFilterPreviousListener( callback ) {
         filterPreviousButton.addEventListener( "click", callback );
+        addKeyEventListener("DOWN", callback);
     }
 
     function addSourceNextListener( callback ) {
         sceneNextButton.addEventListener( "click", callback );
+        addKeyEventListener("LEFT", callback);
     }
 
     function addSourcePreviousListener( callback ) {
         scenePreviousButton.addEventListener( "click", callback );
+        addKeyEventListener("RIGHT", callback);
     }
 
     return {
