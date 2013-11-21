0e9985ee87122bba478a45025c47f03370d052fb
abrie <abrie@errante.local>
2013-11-21 08:45:48 +0000

    remove buttons

--- ui.js
+++ ui.js
@@ -1,17 +1,5 @@
 "use strict";
 define(['keys'], function( keys ) {
-    function makeButton( name ) {
-        var element = document.createElement("button");
-        element.innerHTML = name; 
-        document.body.appendChild( element );
-        return element;
-    }
-
-    var filterPreviousButton = makeButton("previous filter");
-    var filterNextButton = makeButton("next filter");
-    var placePreviousButton = makeButton("previous place");
-    var placeNextButton = makeButton("next place");
-
     var keyEventListeners = {};
     function addKeyEventListener(event, callback) {
         if( !keyEventListeners[event] ) {
@@ -29,22 +17,18 @@ define(['keys'], function( keys ) {
     };
 
     function addFilterNextListener( callback ) {
-        filterNextButton.addEventListener( "click", callback );
         addKeyEventListener("UP", callback);
     }
 
     function addFilterPreviousListener( callback ) {
-        filterPreviousButton.addEventListener( "click", callback );
         addKeyEventListener("DOWN", callback);
     }
 
     function addPlaceNextListener( callback ) {
-        placeNextButton.addEventListener( "click", callback );
         addKeyEventListener("LEFT", callback);
     }
 
     function addPlacePreviousListener( callback ) {
-        placePreviousButton.addEventListener( "click", callback );
         addKeyEventListener("RIGHT", callback);
     }
 
