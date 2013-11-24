e34923814d42ab3f559e671df98b3eb621963a29
abrie <abrie@errante.local>
2013-11-24 19:51:11 +0000

    refactor: DRY noisemaker generators

--- noisemaker.js
+++ noisemaker.js
@@ -1,12 +1,12 @@
 "use strict";
 
 define(['audio'], function( audio ) {
-    function Sine() {
+    function Generator( waveform, burstPattern, fadePattern ) {
         var types = [
             {
                 target: 'oscsynth',
-                notes: [71-12],
-                type: "sine",
+                notes: fadePattern,
+                type: waveform,
                 at: 0,
                 velocity: 1.0,
                 adsr: {attack:1.5, release:1.0 },
@@ -14,8 +14,8 @@ define(['audio'], function( audio ) {
 
             {
                 target: 'oscsynth',
-                notes: [71-12, 64, 68, 71, 71],
-                type: "sine",
+                notes: burstPattern,
+                type: waveform,
                 at: 0,
                 velocity: 1.0,
                 adsr: {attack:0.10, release:0.1 },
@@ -31,70 +31,18 @@ define(['audio'], function( audio ) {
         return {
             emit:emit,
         };
+
+    }
+    function Sine() {
+        return new Generator("sine", [71-12, 64, 68, 71, 71], [71-12] );
     }
 
     function Square() {
-        var types = [
-            {
-                target: 'oscsynth',
-                notes: [71-12],
-                type: "square",
-                at: 0,
-                velocity: 1.0,
-                adsr: {attack:1.0, release:0.2 },
-            },
-
-            {
-                target: 'oscsynth',
-                notes: [71-12, 64, 68],
-                type: "square",
-                at: 0,
-                velocity: 1.0,
-                adsr: {attack:0.10, release:0.1 },
-            }
-        ];
-
-        function emit( lensId, type, span ) {
-            types[type].lensId = lensId;
-            types[type].span = span;
-            audio.dispatch( types[type] );
-        }
-
-        return {
-            emit:emit,
-        };
+        return new Generator("square", [71-12], [71-12, 64, 68] );
     }
 
     function Sawtooth() {
-        var types = [
-            {
-                target: 'oscsynth',
-                notes: [71-12],
-                type: "sawtooth",
-                at: 0,
-                velocity: 1.0,
-                adsr: {attack:1.5, release:2.0 },
-            },
-
-            {
-                target: 'oscsynth',
-                notes: [71-12, 71-11, 71-10, 71-6, 71-3 ],
-                type: "sawtooth",
-                at: 0,
-                velocity: 1.0,
-                adsr: {attack:0.10, release:0.1 },
-            }
-        ];
-
-        function emit( lensId, type, span ) {
-            types[type].lensId = lensId;
-            types[type].span = span;
-            audio.dispatch( types[type] );
-        }
-
-        return {
-            emit:emit,
-        };
+        return new Generator("sawtooth", [71-12, 64, 68, 71, 71], [71-12] );
     }
 
     return {
